7f15b520a7ef80a79ac58db7bb26c30b5f27b2ec
ShinDongHyeop <unfade2@gmail.com>
2016-10-09 17:17:12 +0900

    Stage2, Stage3 Add
    Background, Ground Add

--- Map.py
+++ Map.py
@@ -1,9 +1,28 @@
 from pico2d import *
+import os
 
-class Background:
+class Stage1_Background:
     def __init__(self):
         self.Background = load_image('Resource\\Map\\Stage1\\First_Background.png')
         self.Ground = load_image('Resource\\Map\\Stage1\\First_ground.png')
+
+    def draw(self):
+        self.Background.draw(400, 200)
+        self.Ground.draw(400, 350)
+
+class Stage2_Background:
+    def __init__(self):
+        self.Background = load_image('Resource\\Map\\Stage2\\Second_Background.png')
+        self.Ground = load_image('Resource\\Map\\Stage2\\Second_ground.png')
+
+    def draw(self):
+        self.Background.draw(400, 200)
+        self.Ground.draw(400, 350)
+
+class Stage3_Background:
+    def __init__(self):
+        self.Background = load_image('Resource\\Map\\Stage3\\Third_Background.png')
+        self.Ground = load_image('Resource\\Map\\Stage3\\Third_ground.png')
 
     def draw(self):
         self.Background.draw(400, 200)
