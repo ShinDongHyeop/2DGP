876d0729c76d1fbc410571f9a205cb83149af337
ShinDongHyeop <unfade2@gmail.com>
2016-10-31 23:13:11 +0900

    Item 모듈 생성 및 Stage1 점수젤리 클래스 생성
    stage1 점수젤리 클래스 생성

---
+++ item.py
@@ -0,0 +1,56 @@
+import random
+import json
+from pico2d import *
+
+class Stage1_SPEED:
+    PIXEL_PER_METER = (10.0 / 0.3)
+    RUN_SPEED_KMPH = 20.0
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+class Stage2_SPEED:
+    PIXEL_PER_METER = (10.0 / 0.3)
+    RUN_SPEED_KMPH = 30.0
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+class Stage3_SPEED:
+    PIXEL_PER_METER = (10.0 / 0.3)
+    RUN_SPEED_KMPH = 30.0
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+class Stage4_SPEED:
+    PIXEL_PER_METER = (10.0 / 0.3)
+    RUN_SPEED_KMPH = 40.0
+    RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+    RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+    RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+class Stage1_Item_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.collision_time = 0
+        if Stage1_Item_Jelly.image == None:
+            self.Item_Jelly = load_image('Resource\\Item\\item_Jelly.png')
+
+    def update(self, frame_time):
+        if Stage1_SPEED.RUN_SPEED_PPS * frame_time < 12:
+            self.distance = Stage1_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+            self.collision_time = 0
+
+    def draw(self):
+        self.Item_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
