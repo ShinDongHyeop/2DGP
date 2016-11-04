241695fded8a53f361a217204e423196d899ce3e
ShinDongHyeop <unfade2@gmail.com>
2016-11-04 20:06:09 +0900

    stage 2,3,4 item class 추가
    stage 2, 3, 4의 Jelly와 Hp_jelly class 추가
    main_state의 draw및 update는 미구현

--- item.py
+++ item.py
@@ -97,3 +97,189 @@ class Stage1_Hp_Jelly:
 
     def get_bb(self):
         return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+class Stage2_Item_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = random.randint(300, 15000)
+        self.y = random.randint(170, 300)
+        if Stage2_Item_Jelly.image == None:
+            self.Item_Jelly = load_image('Resource\\Item\\item_Jelly.png')
+
+    def update(self, frame_time):
+        if Stage2_SPEED.RUN_SPEED_PPS * frame_time < 18:
+            self.distance = Stage2_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+
+    def draw(self):
+        self.Item_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+class Stage2_Hp_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        if Stage2_Hp_Jelly.image == None:
+            self.Hp_Jelly = load_image('Resource\\Item\\hp_jelly.png')
+
+    def create(self):
+        hp_state_table = {
+            "Hp" : self.Hp_Jelly
+        }
+
+        hp = []
+        for name in hp_jelly:
+            item = Stage2_Hp_Jelly()
+            item.name = name
+            item.x = hp_jelly[name]['x']
+            item.y = hp_jelly[name]['y']
+            item.state = hp_state_table[hp_jelly[name]['state']]
+            hp.append(item)
+
+        return hp
+
+    def update(self, frame_time):
+        if Stage2_SPEED.RUN_SPEED_PPS * frame_time < 18:
+            self.distance = Stage2_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+
+    def draw(self):
+        self.Hp_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+class Stage3_Item_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = random.randint(300, 15000)
+        self.y = random.randint(170, 300)
+        if Stage3_Item_Jelly.image == None:
+            self.Item_Jelly = load_image('Resource\\Item\\item_Jelly.png')
+
+    def update(self, frame_time):
+        if Stage3_SPEED.RUN_SPEED_PPS * frame_time < 24:
+            self.distance = Stage3_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+
+    def draw(self):
+        self.Item_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+class Stage3_Hp_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        if Stage3_Hp_Jelly.image == None:
+            self.Hp_Jelly = load_image('Resource\\Item\\hp_jelly.png')
+
+    def create(self):
+        hp_state_table = {
+            "Hp" : self.Hp_Jelly
+        }
+
+        hp = []
+        for name in hp_jelly:
+            item = Stage3_Hp_Jelly()
+            item.name = name
+            item.x = hp_jelly[name]['x']
+            item.y = hp_jelly[name]['y']
+            item.state = hp_state_table[hp_jelly[name]['state']]
+            hp.append(item)
+
+        return hp
+
+    def update(self, frame_time):
+        if Stage3_SPEED.RUN_SPEED_PPS * frame_time < 24:
+            self.distance = Stage3_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+
+    def draw(self):
+        self.Hp_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+class Stage4_Item_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = random.randint(300, 18700)
+        self.y = random.randint(170, 300)
+        if Stage4_Item_Jelly.image == None:
+            self.Item_Jelly = load_image('Resource\\Item\\item_Jelly.png')
+
+    def update(self, frame_time):
+        if Stage4_SPEED.RUN_SPEED_PPS * frame_time < 24:
+            self.distance = Stage4_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+
+    def draw(self):
+        self.Item_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
+
+class Stage4_Hp_Jelly:
+    image = None
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        if Stage4_Hp_Jelly.image == None:
+            self.Hp_Jelly = load_image('Resource\\Item\\hp_jelly.png')
+
+    def create(self):
+        hp_state_table = {
+            "Hp" : self.Hp_Jelly
+        }
+
+        hp = []
+        for name in hp_jelly:
+            item = Stage4_Hp_Jelly()
+            item.name = name
+            item.x = hp_jelly[name]['x']
+            item.y = hp_jelly[name]['y']
+            item.state = hp_state_table[hp_jelly[name]['state']]
+            hp.append(item)
+
+        return hp
+
+    def update(self, frame_time):
+        if Stage4_SPEED.RUN_SPEED_PPS * frame_time < 12:
+            self.distance = Stage4_SPEED.RUN_SPEED_PPS * frame_time
+            self.x -= self.distance
+
+    def draw(self):
+        self.Hp_Jelly.draw(self.x, self.y)
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 20, self.y - 20, self.x + 20, self.y + 20
